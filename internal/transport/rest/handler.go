d8791d0dd19cf11bb13d11061b9127ee2c2d0000
Danil <58632836+WB31B@users.noreply.github.com>
2023-07-07 13:12:30 +0300

    Update handler.go

--- internal/transport/rest/handler.go
+++ internal/transport/rest/handler.go
@@ -10,12 +10,14 @@ import (
 func HandleFunction() {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/", services.Index).Methods("GET")
-	rtr.HandleFunc("/create/", services.Create).Methods("GET")
-	rtr.HandleFunc("/save_article", services.Save_article).Methods("POST")
-	rtr.HandleFunc("/tickets/", services.Tickets).Methods("GET")
-	rtr.HandleFunc("/ticket/{id:[0-9]+}", services.Show_ticket).Methods("GET")
-	rtr.HandleFunc("/create_ticket", services.Create_ticket).Methods("GET")
-	rtr.HandleFunc("/show_form", services.Show_form).Methods("GET")
+	rtr.HandleFunc("/create_ticket", services.CreateTicket).Methods("GET")
+	rtr.HandleFunc("/create_note", services.CreateNote).Methods("GET")
+	rtr.HandleFunc("/save_ticket", services.SaveTicket).Methods("POST")
+	rtr.HandleFunc("/save_note", services.SaveNote).Methods("POST")
+	rtr.HandleFunc("/show_tickets", services.ShowTickets).Methods("GET")
+	rtr.HandleFunc("/show_notes", services.ShowNotes).Methods("GET")
+	rtr.HandleFunc("/ticket/{id:[0-9]+}", services.ShowTicket).Methods("GET")
+	rtr.HandleFunc("/note/{id:[0-9]+}", services.ShowNote).Methods("GET")
 
 	http.Handle("/", rtr)
 
