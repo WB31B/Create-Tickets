fbc7e1041fb487b3a592158057913cda77fd5179
Danil <58632836+WB31B@users.noreply.github.com>
2023-07-07 13:12:54 +0300

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,28 @@
 package config
 
-type Article struct {
+type Tickets struct {
 	Id                            uint16
 	Username, Information, Ticket string
 }
 
-type Page struct {
-	Article      []Article
+type Notes struct {
+	Id                 uint16
+	Title, Description string
+}
+
+type PageTickets struct {
+	Tickets      []Tickets
 	CurrentPage  int
 	TotalPage    int
 	CountTickets int
 }
 
-var Inforamations = []Article{}
-var ShowTicket = Article{}
+type PageNotes struct {
+	Notes        []Notes
+	CurrentPage  int
+	TotalPage    int
+	CountTickets int
+}
+
+// var Inforamations = []Tickets{}
+// var ShowTicket = Tickets{}
