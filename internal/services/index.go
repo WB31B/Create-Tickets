cd33a4becddee7acb6ebce18231a9fbefcdda0ee
Danil <58632836+WB31B@users.noreply.github.com>
2023-07-17 15:25:28 +0300

    Update index.go

--- internal/services/index.go
+++ internal/services/index.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"root/root/database"
+	"root/root/errors"
+	"root/root/internal/config"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -12,5 +15,25 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, err.Error())
 	}
 
-	t.ExecuteTemplate(w, "index", nil)
+	countQuery := "select count(*) from notes"
+	var totalNotes int
+	err = database.DB.QueryRow(countQuery).Scan(&totalNotes)
+	errors.CheckError(err)
+
+	rows, err := database.DB.Query(fmt.Sprintf("select * from notes where id_note = '%d'", totalNotes))
+	errors.CheckError(err)
+
+	showNote := config.Notes{}
+
+	for rows.Next() {
+		var post config.Notes
+		err := rows.Scan(&post.Id, &post.Title, &post.Description)
+		if err != nil {
+			continue
+		}
+
+		showNote = post
+	}
+
+	t.ExecuteTemplate(w, "index", showNote)
 }
